e72ce137fbe214f536b461c8429b580dec9f8579
Bonface Masira <bonfacemasira@gmail.com>
2022-06-23 13:14:45 +0300

    commit tree 6158688d5831f7f056b3af7c88f43561c9aec7f0

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your code here
+class Breakfast {
+    constructor(food, drink){
+        this.food = food;
+        this.drink = drink;
+    }
+}
+
+class Lunch {
+    constructor(salad, soup, drink){
+        this.salad = salad;
+        this.soup = soup;
+        this.drink = drink;
+    }
+}
+
+class Dinner {
+    #dessert;
+    constructor(salad, soup, entree, dessert){
+        this.#dessert = dessert;
+        this.salad = salad;
+        this.soup = soup;
+        this.entree = entree;
+    }
+}
+
+let breakfast = new Breakfast("eggs","juice");
+let lunch = new Lunch("side salad","broccoli cheddar soup","iced tea");
+let dinner = new Dinner("balsamic salad","consomme","filet mignon","cheesecake");
